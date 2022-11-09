77bba45f18062122bda6c66071413a9f309b7e14
Álvaro <95368497+ALVareador@users.noreply.github.com>
2022-11-09 10:45:45 +0100

    se vienen cositas

--- app/src/main/java/org/uvigo/dm2_juego_del_ajedrez/MainActivity.java
+++ app/src/main/java/org/uvigo/dm2_juego_del_ajedrez/MainActivity.java
@@ -3,6 +3,8 @@ package org.uvigo.dm2_juego_del_ajedrez;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,47 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Button newGame = findViewById(R.id.BotonNuevaPartida);
+        Button continueGame = findViewById(R.id.botonContinuarPartida);
+        Button friends = findViewById(R.id.botonAmigos);
+        Button credits = findViewById(R.id.botonCreditos);
+        Button exit = findViewById(R.id.botonSalir);
+
+        newGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        continueGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        friends.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        credits.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        exit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
---
+++ app/src/main/java/org/uvigo/dm2_juego_del_ajedrez/ProfileActivity.java
@@ -0,0 +1,17 @@
+package org.uvigo.dm2_juego_del_ajedrez;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+    }
+}
